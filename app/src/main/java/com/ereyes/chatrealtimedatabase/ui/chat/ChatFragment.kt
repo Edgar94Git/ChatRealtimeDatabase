d917e07ffd4a00e21c145420021a60576991f311
Edgar Reyes <gzzenit@gmail.com>
2024-05-05 20:25:48 -0600

    se agrega fragment chat

---
+++ app/src/main/java/com/ereyes/chatrealtimedatabase/ui/chat/ChatFragment.kt
@@ -0,0 +1,22 @@
+package com.ereyes.chatrealtimedatabase.ui.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ereyes.chatrealtimedatabase.R
+import com.ereyes.chatrealtimedatabase.databinding.FragmentChatBinding
+
+class ChatFragment : Fragment() {
+
+    private lateinit var binding: FragmentChatBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
